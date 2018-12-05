117e6b30f4c4c50abef5135bd132540b8f1eed49
gregory.gotlib <gregory.gotlib@gmail.com>
2018-12-05 17:07:22 +0200

    Updates

--- precache-manifest.f5e1a6fab34642b7f514bd5ca17eb404.js
+++ precache-manifest.f5e1a6fab34642b7f514bd5ca17eb404.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/GregoryGotlib/Chatter-.git/static/js/runtime~main.308ec7d2.js"
   },
   {
-    "revision": "0d4d0630770119eac64b",
-    "url": "/GregoryGotlib/Chatter-.git/static/js/main.0d4d0630.chunk.js"
+    "revision": "3d238c47bd1edf022e80",
+    "url": "/GregoryGotlib/Chatter-.git/static/js/main.3d238c47.chunk.js"
   },
   {
     "revision": "d07ffe130b04030ba81d",
     "url": "/GregoryGotlib/Chatter-.git/static/js/1.d07ffe13.chunk.js"
   },
   {
-    "revision": "0d4d0630770119eac64b",
+    "revision": "3d238c47bd1edf022e80",
     "url": "/GregoryGotlib/Chatter-.git/static/css/main.c5b54fcd.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/GregoryGotlib/Chatter-.git/static/css/1.01b649cf.chunk.css"
   },
   {
-    "revision": "b702632b32dd3e49fac107bd051bce05",
+    "revision": "bbdd54d379de1a1506f9c9b81f0b51ea",
     "url": "/GregoryGotlib/Chatter-.git/index.html"
   }
 ];
